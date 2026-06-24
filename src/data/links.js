b63a9856779035e40cc66e95a53eedf9b7af2c42
Hristian Ivanow <hristianfancha@gmail.com>
2026-06-24 09:00:36 +0300

    chore: centralize cv link

--- src/data/links.js
+++ src/data/links.js
@@ -2,7 +2,6 @@ export const links = {
     github: "https://github.com/hristianivanov",
     linkedin: "https://www.linkedin.com/in/hristian-ivanov",
     email: "mailto:hristianfancha@gmail.com",
-    // Central CV link. The PDF is synced through Dropbox and rendered directly in the browser.
     cv: "https://www.dropbox.com/scl/fi/zt7hec4b9u3lr4j2baw3p/hristian-ivanov-cv.pdf?rlkey=o5ej9plxyrjoqnnw2a0rh9rk4&raw=1",
     softUni: "https://softuni.bg/",
 };
