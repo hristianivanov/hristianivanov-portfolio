4e723736e9518f1b55e81b898a7a14c446cfc27f
Hristian Ivanow <hristianfancha@gmail.com>
2026-06-24 09:17:26 +0300

    clean comment

--- src/data/links.js
+++ src/data/links.js
@@ -2,7 +2,6 @@ export const links = {
     github: "https://github.com/hristianivanov",
     linkedin: "https://www.linkedin.com/in/hristian-ivanov",
     email: "mailto:hristianfancha@gmail.com",
-    // Stable public CV URL. Netlify redirects this path to the synced Dropbox PDF.
     cv: "https://hristianivanov.netlify.app/cv",
     softUni: "https://softuni.bg/",
 };
